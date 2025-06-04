5ef32c44c385db7ccd6bffdbabf26ece0989c7f9
jorgevg0 <jorge.velazquez.gomez@gmail.com>
2025-06-04 13:27:10 +0200

    Actualizacion de funciones en procesamiento a descriptores

--- scripts/Procesamiento_descriptores/eliminar_columnas_alta_correlacion.py
+++ scripts/Procesamiento_descriptores/eliminar_columnas_alta_correlacion.py
@@ -1,11 +1,16 @@
 import numpy as np
 import pandas as pd
 
-def eliminar_columnas_alta_correlacion(descriptores, threshold):
-    matriz_cor = descriptores.iloc[:,1:].corr().abs()
-    matriz_cor_superior = matriz_cor.where(np.triu(np.ones(matriz_cor.shape), k=1).astype(bool))
-    columnas_alta_correlacion = [column for column in matriz_cor_superior.columns if any(matriz_cor_superior[column] > threshold)]
+def eliminar_columnas_alta_correlacion (descriptores,threshold):
+    matriz_cor= descriptores.iloc[:,1:].corr().abs() # Obtenemos la matriz de correlación en términos absolutos
+    matriz_cor_superior=matriz_cor.where(np.triu(np.ones(matriz_cor.shape),k=1).astype(bool)) # Necesitamos solo la parte superior de la matriz
+    #np.ones(matriz_cor.shape): Crea una matriz del mismo tamaño llena de 1s.
+    #np.triu(..., k=1): Extrae la parte superior de la matriz (por encima de la diagonal principal).
+    #.astype(bool): Convierte la matriz en una de True/False, donde la parte superior es True y el resto False.
+    #matriz_cor.where(...): Sustituye los valores de matriz_cor donde True, y pone NaN donde False.
+    #Fuente:https://stackoverflow.com/questions/17778394/list-highest-correlation-pairs-from-a-large-correlation-matrix-in-pandas
+    columnas_alta_correlacion=[column for column in matriz_cor_superior.columns if any(matriz_cor_superior[column]>threshold)]
     print(columnas_alta_correlacion)
-    print("Numero de columnas con alta correlación:", len(columnas_alta_correlacion))
-    descriptores_correlacionados_eliminados = descriptores.drop(columns=columnas_alta_correlacion)
+    print("Numero de columnas con alta correlación:",len(columnas_alta_correlacion))
+    descriptores_correlacionados_eliminados=descriptores.drop(columns=columnas_alta_correlacion)
     return descriptores_correlacionados_eliminados
--- scripts/Procesamiento_descriptores/eliminar_columnas_baja_varianza.py
+++ scripts/Procesamiento_descriptores/eliminar_columnas_baja_varianza.py
@@ -2,18 +2,24 @@ import pandas as pd
 from sklearn.feature_selection import VarianceThreshold
 
 def eliminar_baja_varianza(datos, threshold=0.1):
-    datos_sin_id_respuesta = datos.iloc[:,1:-1]
-    selection = VarianceThreshold(threshold)
-    selection.fit(datos_sin_id_respuesta)
+    datos_sin_id_respuesta=datos.iloc[:,1:-1]
+    selection = VarianceThreshold(threshold)  # Se crea una instancia de VarianceThreshold con el umbral
+    selection.fit(datos_sin_id_respuesta)  # Calculamos la varianza de cada columna en datos
 
+    # Obtener las columnas que se conservan
     columnas_conservadas = datos_sin_id_respuesta.columns[selection.get_support(indices=True)]
+
+    # Identificar las columnas eliminadas (baja varianza)
     columnas_eliminadas = [col for col in datos_sin_id_respuesta.columns if col not in columnas_conservadas]
 
+    # Mostrar las columnas eliminadas
     if columnas_eliminadas:
         print(f"Se eliminaron {len(columnas_eliminadas)} columnas por baja varianza (< {threshold}): {columnas_eliminadas}")
     else:
         print(f"No se eliminaron columnas con baja varianza < {threshold}")
 
-    datos_baja_varianza = pd.concat([datos[['Molecule ChEMBL ID']], datos[columnas_conservadas], datos[['Standard Value']]], axis=1)
+    datos_baja_varianza=pd.concat([datos[['Molecule ChEMBL ID']], datos[columnas_conservadas], datos[['Standard Value']]], axis=1)
 
     return datos_baja_varianza
+
+#Código basado en:https://github.com/herrynguyen1706/Selecting-the-best-molecular-descriptor/blob/main/Cleaning_Molecular_descriptors.ipynb
